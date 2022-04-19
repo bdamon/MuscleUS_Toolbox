86e691358bed3cb4db4458ee93d797404d75a8e2
Bruce Damon <bruce.damon@carle.com>
2022-04-19 05:56:03 -0500

    Update fiber_visualizer_us.m

--- Matlab-Functions/fiber_visualizer_us.m
+++ Matlab-Functions/fiber_visualizer_us.m
@@ -4,29 +4,31 @@ function us_figure = fiber_visualizer_us(image_doub, fv_options, fiber_all, roi_
 %  us_figure = fiber_visualizer_us(image_doub, fv_options, fiber_all, roi_struc);
 %
 %USAGE
-%  The function fiber_ visualizer_us is used to visualize ultrasound images and 
-%  other structures, including the muscle mask, aponeurosis definition, and/or the 
-%  fiber tracts.
-%
-%  The user can call fiber_visualizer from the command line.  In addition, 
-%  read_dicom_us, fiber_track, and fiber_smoother_us, can be configured to call 
-%  fiber_visualizer from within the functions, so that the image, mask, 
-%  aponeurosis definition, and fiber tracts can be automatically plotted.  The 
-%  user supplies a double precision image, a structure with some plotting 
-%  options, and the other variables to be plotted as input arguments. A handle 
-%  to the figure is returned.
+%  The function fiber_visualizer_us is used to fiber-tracts in the MuscleUS_Toolbox.
+%  The user provides a double precision B-mode image, a matrix of fiber-tracts,
+%  a structure containing ROI definitions, and a structure containing
+%  visualization options.
 %
 %INPUT ARGUMENTS
 %  image_doub: A grayscale, B-mode image at double-precision
 %
 %  fv_options: A structure with visualization options, as defined in the
 %  following fields:
-%    -.tract_color: If tract_color is a 1x3 vector, the tracts will be
-%      plotted as a single color according to an RGB coding mechanism. If
-%      tract_color is an Mx3 matrix (with M=the number of fiber tracts),
-%      each tract will be plotted using a different color
-%    -.roi_color: A 1x3 vector used to indicate the color of the
-%      aponeurosis region of interest
+%    -.plot_tracts: If set to 1, the fiber tracts will be plotted; other-
+%       wise, set to 0
+%    -.plot_mask: If set to 1, the muscle ROI will be plotted; other-
+%       wise, set to 0
+%    -.plot_roi: If set to 1, the aponeurosis ROI will be plotted; other-
+%       wise, set to 0
+%    -.tract_color: Required when plot_tracts = 1. If tract_color is a 1x3 
+%       vector, the tracts will be plotted as a single color according to an
+%       RGB coding mechanism. If tract_color is an Mx3 matrix (with M = the 
+%       number of fiber tracts), each tract will be plotted using a 
+%       different color
+%    -.mask_color: Required when plot_mask = 1. A 1x3 vector used to 
+%       indicate the color of the muscle region of interest
+%    -.roi_color: Required when plot_roi = 1. A 1x3 vector used to indicate 
+%       the color of the aponeurosis region of interest
 %
 %  fiber_all (optional): The fiber tracts to be displayed, if desired
 %
@@ -46,14 +48,14 @@ function us_figure = fiber_visualizer_us(image_doub, fv_options, fiber_all, roi_
 %% Plot fiber tracts
 
 %define figure window
-us_figure = figure('units', 'normalized', 'position', [0.1 .1 .8 .75]);
+us_figure = figure('units', 'normalized', 'position', [0.1 .1 .6 .75]);
 imagesc(image_doub), colormap gray
 axis image
 hold on
 
 
-if exist('fiber_all', 'var')
-    for k = 2:(length(fiber_all(:,1,1))-1)
+if fv_options.plot_tracts==1
+    for k = 1:length(fiber_all(:,1,1))
 
         %get fiber tract poins
         loop_c = nonzeros(squeeze(fiber_all(k,:,2)));
@@ -61,18 +63,20 @@ if exist('fiber_all', 'var')
 
         %plot using user-defined color(s)
         if numel(fv_options.tract_color)==3
-            plot(loop_c, loop_r, 'color', fv_options.tract_color)
+            plot(loop_c, loop_r, fv_options.tract_color)
         elseif numel(fv_options.tract_color)>3
-            plot(loop_c, loop_r, 'color', fv_options.tract_color(k,:))
+            plot(loop_c, loop_r, fv_options.tract_color(k,:))
         end
 
     end
 end
 
-if exist('roi_struc', 'var')
-    for k = 1:length(fiber_all(:,1,1))
-        plot(roi_struc.fitted_roi_c_pixels, roi_struc.fitted_roi_r_pixels, 'color', fv_options.roi_color)
-    end
+if fv_options.plot_roi==1
+   plot(roi_struc.fitted_roi_c_pixels, roi_struc.fitted_roi_r_pixels, 'color', fv_options.roi_color)
+end
+
+if fv_options.plot_mask==1
+    plot(roi_struc.muscle_c_pixels, roi_struc.muscle_r_pixels, 'color', fv_options.mask_color)
 end
 
 
